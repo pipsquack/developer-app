5b4ef385b9945eb4b04cc23abfc4ce7cd258dbe4
Alex Fernandes <alex.fernandes@datadoghq.com>
2023-10-28 11:53:17 -0400

    First commit

---
+++ app/src/main/java/com/datadog/ese/sandbox/scenario/Boom.java
@@ -0,0 +1,29 @@
+package com.datadog.ese.sandbox.scenario;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.LoggerFactory;
+
+@WebServlet("/boom")
+public class Boom extends HttpServlet {
+
+  final static org.slf4j.Logger logger = LoggerFactory.getLogger("sandbox");
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    resp.setContentType("text/html");
+    try {
+      System.out.println(5 / 0);
+    } catch (RuntimeException e) {
+      logger.error(e.getMessage(), e);
+    }
+    resp.getWriter().println("<h1>Hello world!</h1>");
+  }
+
+}
