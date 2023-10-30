2700fbeaf40c86a50a529d1899cccceb5da949d8
Alex Fernandes <alex.fernandes@datadoghq.com>
2023-10-30 09:38:09 -0400

    Handling for reponses not 2xx

--- app/src/main/java/com/example/springboot/LoadController.java
+++ app/src/main/java/com/example/springboot/LoadController.java
@@ -8,6 +8,7 @@ import org.apache.hc.core5.http.ClassicHttpRequest;
 import org.apache.hc.core5.http.io.support.ClassicRequestBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -36,22 +37,29 @@ public class LoadController {
         ResponseEntity<String> response = null;
         try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
             response = (ResponseEntity<String>) httpclient.execute(httpGet, r -> {
-                JsonNode json = mapper.readTree(r.getEntity().getContent());
-                JsonNode data = json.get("data");
-                JsonNode top = null;
-                for (JsonNode jsonNode : data) {
-                    if (top == null) {
-                        top = jsonNode;
-                    } else {
-                        if (Integer.parseInt(jsonNode.get("value").textValue()) > Integer.parseInt(top.get("value").textValue())) {
+                if (HttpStatus.valueOf(r.getCode()).is2xxSuccessful()) {                    
+                    JsonNode json = mapper.readTree(r.getEntity().getContent());
+                    JsonNode data = json.get("data");
+                    JsonNode top = null;
+                    for (JsonNode jsonNode : data) {
+                        if (top == null) {
                             top = jsonNode;
+                        } else {
+                            if (Integer.parseInt(jsonNode.get("value").textValue()) > Integer
+                                    .parseInt(top.get("value").textValue())) {
+                                top = jsonNode;
+                            }
                         }
                     }
+                    logger.info("Highest value credit card is {}", top.get("cc").textValue());
+                    return new ResponseEntity<String>(json.toString(), HttpStatusCode.valueOf(r.getCode()));
+                } else {
+                    return new ResponseEntity<String>(r.getReasonPhrase(), HttpStatusCode.valueOf(r.getCode()));
                 }
-                logger.info("Highest value credit card is {}", top.get("cc").textValue());
-                return new ResponseEntity<String>(json.toString(), HttpStatusCode.valueOf(r.getCode()));
             });
-        } catch (IOException e) {
+        } catch (
+
+        IOException e) {
             e.printStackTrace();
         }
         return response;
