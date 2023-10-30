8af564938029b7c533ca94759932cd8ef49a2fa9
Alex Fernandes <alex.fernandes@datadoghq.com>
2023-10-30 16:09:50 -0400

    Adding vulnerability

--- app/src/main/java/com/example/springboot/LoadController.java
+++ app/src/main/java/com/example/springboot/LoadController.java
@@ -1,6 +1,7 @@
 package com.example.springboot;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
@@ -14,14 +15,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 @RestController
 public class LoadController {
 
     Logger logger = LoggerFactory.getLogger("developer-app");
-    ObjectMapper mapper = new ObjectMapper();
     private ClassicHttpRequest httpGet;
 
     public LoadController() {
@@ -37,21 +39,24 @@ public class LoadController {
         ResponseEntity<String> response = null;
         try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
             response = (ResponseEntity<String>) httpclient.execute(httpGet, r -> {
-                if (HttpStatus.valueOf(r.getCode()).is2xxSuccessful()) {                    
-                    JsonNode json = mapper.readTree(r.getEntity().getContent());
-                    JsonNode data = json.get("data");
-                    JsonNode top = null;
-                    for (JsonNode jsonNode : data) {
-                        if (top == null) {
-                            top = jsonNode;
-                        } else {
-                            if (Integer.parseInt(jsonNode.get("value").textValue()) > Integer
-                                    .parseInt(top.get("value").textValue())) {
-                                top = jsonNode;
+                if (HttpStatus.valueOf(r.getCode()).is2xxSuccessful()) {
+                    JsonObject json = (JsonObject) JsonParser.parseReader(new InputStreamReader(r.getEntity().getContent()));
+                    JsonArray data = json.get("data").getAsJsonArray();
+                    JsonObject top = null;
+                    for (JsonElement element : data) {
+                        if (element instanceof JsonObject) {
+                            JsonObject node = (JsonObject) element;
+                            if (top == null) {
+                                top = node;
+                            } else {
+                                if (Integer.parseInt(node.get("value").getAsString()) > Integer
+                                        .parseInt(top.get("value").getAsString())) {
+                                    top = node;
+                                }
                             }
                         }
                     }
-                    logger.info("Highest value credit card is {}", top.get("cc").textValue());
+                    logger.info("Highest value credit card is {}", top.get("cc").getAsString());
                     return new ResponseEntity<String>(json.toString(), HttpStatusCode.valueOf(r.getCode()));
                 } else {
                     return new ResponseEntity<String>(r.getReasonPhrase(), HttpStatusCode.valueOf(r.getCode()));
