94955e936382b4f2eaea850323f570c5abc9ab5f
Alex Fernandes <alex.fernandes@datadoghq.com>
2024-06-04 13:16:37 -0400

    Makes Flaky test unskippable

--- app/src/test/java/com/example/springboot/FlakyTest.java
+++ app/src/test/java/com/example/springboot/FlakyTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.security.SecureRandom;
 
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -13,7 +15,8 @@ public class FlakyTest {
     SecureRandom rand = new SecureRandom();
 
     @Test
-    public void getHello() throws Exception {
+    @Tags({@Tag("datadog_itr_unskippable")})
+    public void roll_the_dice() throws Exception {
         assertThat(rand.nextBoolean()).isTrue();
     }
 }
